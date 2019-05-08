3f50cc0dab59d22a03404c57b02bf9bcdacb7f2d
MeetDevin <meetdevin.zh@outlook.com>
2019-05-08 22:35:36 +0800

    Update draw_pic.py

--- drawing_place/draw_pic.py
+++ drawing_place/draw_pic.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import cv2
 #
 # conda install -c conda-forge librosa
-y, sr = librosa.load('D:/GitHub/ProjectX/sounds_data/mp3/Oriolus+oriolus/Oriolus oriolus_240851.mp3', sr=44100)
+y, sr = librosa.load('D:/GitHub/ProjectX/sounds_data/mp3/Oriolus+oriolus/Oriolus oriolus_240866.mp3', sr=44100)
 t = len(y) / sr
 print(sr, t)
 
@@ -39,7 +39,7 @@ def stack_features(y, frames=600):
     return features3d
 
 
-signal = stack_features(y)[0]
+signal = stack_features(y)[45]
 mel = librosa.feature.melspectrogram(y=signal, sr=sr, n_mels=80, n_fft=1024, hop_length=512, power=2.0)
 logspec = librosa.amplitude_to_db(mel)
 
@@ -47,22 +47,22 @@ logspec = librosa.amplitude_to_db(mel)
 plt.figure()
 plt.subplot(3, 1, 1)
 
-# librosa.display.specshow(logspec, y_axis='mel', fmax=44100, x_axis='time')
+librosa.display.specshow(logspec, y_axis='mel', fmax=44100, x_axis='time')
 
-# librosa.display.waveplot(signal, sr, x_axis='time')
-fig = plt.figure(figsize=(10, 4))
-ax1 = fig.add_subplot(111)
-time = np.arange(0, len(signal)) * (1.0 / sr)
-ax1.plot([i for i in time], [value for value in signal], 'b', label='waveform')
-# 设置刻度字体大小
-plt.xticks(fontsize=18)
-plt.yticks(fontsize=18)
-ax1.set_xlabel("Time(s)", fontsize=18)
-ax1.set_ylabel('Amplitude(m)', fontsize=18)
-plt.legend(loc='lower right', fontsize=18)
-
+# fig = plt.figure(figsize=(10, 4))
+# ax1 = fig.add_subplot(111)
+# time = np.arange(0, len(signal)) * (1.0 / sr)
+# ax1.plot([i for i in time], [value for value in signal], 'b', label='Oriolus oriolus')
+# # 设置刻度字体大小
+# plt.xticks(fontsize=18)
+# plt.yticks(fontsize=18)
+# ax1.set_xlabel("Time(s)", fontsize=18)
+# ax1.set_ylabel('Amplitude(mm)', fontsize=18)
+# plt.legend(loc='lower right', fontsize=18)
 
 
-# plt.colorbar()
+plt.colorbar()
+plt.xlabel('Time')
+plt.ylabel('Mel scale(mel)')
 # plt.tight_layout()
 plt.show()
