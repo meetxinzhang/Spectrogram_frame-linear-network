2db832b135b36a2b997f9afd4318f29b6f09a5c2
MeetDevin <meetdevin.zh@outlook.com>
2019-05-27 17:29:56 +0800

    双层LSTM用于分类

--- model.py
+++ model.py
@@ -14,18 +14,18 @@ class The3dcnn_lstm_Model(tf.keras.Model):
                                               kernel_initializer=tf.keras.initializers.he_normal(),
                                               bias_initializer=tf.zeros_initializer(),
                                               data_format='channels_last')
-        self.pooling1 = tf.keras.layers.MaxPool3D(pool_size=[3, 2, 2], strides=[3, 2, 2], padding='same',
+        self.pooling1 = tf.keras.layers.MaxPool3D(pool_size=[2, 2, 2], strides=[2, 2, 2], padding='same',
                                                   data_format='channels_last')
 
-        self.conv3d2 = tf.keras.layers.Conv3D(filters=16, kernel_size=[3, 3, 3], strides=[1, 1, 1], use_bias=True,
+        self.conv3d2 = tf.keras.layers.Conv3D(filters=12, kernel_size=[3, 3, 3], strides=[1, 1, 1], use_bias=True,
                                               activation=tf.nn.leaky_relu, padding='same',
                                               kernel_initializer=tf.keras.initializers.he_normal(),
                                               bias_initializer=tf.zeros_initializer(),
                                               data_format='channels_last')
-        self.pooling2 = tf.keras.layers.MaxPool3D(pool_size=[3, 2, 2], strides=[3, 2, 2], padding='same',
+        self.pooling2 = tf.keras.layers.MaxPool3D(pool_size=[2, 2, 2], strides=[2, 2, 2], padding='same',
                                                   data_format='channels_last')
 
-        self.conv3d3 = tf.keras.layers.Conv3D(filters=4, kernel_size=[3, 3, 3], strides=[1, 1, 1], use_bias=True,
+        self.conv3d3 = tf.keras.layers.Conv3D(filters=8, kernel_size=[3, 3, 3], strides=[1, 1, 1], use_bias=True,
                                               activation=tf.nn.leaky_relu, padding='same',
                                               kernel_initializer=tf.keras.initializers.he_normal(),
                                               bias_initializer=tf.zeros_initializer(),
@@ -41,6 +41,10 @@ class The3dcnn_lstm_Model(tf.keras.Model):
         # self.pooling4 = tf.keras.layers.MaxPool3D(pool_size=[3, 1, 1], strides=[3, 1, 1], padding='same',
         #                                           data_format='channels_last')
 
+        self.cell1 = tf.keras.layers.CuDNNLSTM(units=40, return_sequences=True)
+        self.cell2 = tf.keras.layers.CuDNNLSTM(units=self.num_class)
+        # drop = tf.keras.layers.Dropout(rate=drop_rate)
+
         # FC
         # self.fla = tf.keras.layers.Flatten(data_format='channels_last')
         # self.fc1 = tf.keras.layers.Dense(units=128, use_bias=True, activation=None,
@@ -54,14 +58,14 @@ class The3dcnn_lstm_Model(tf.keras.Model):
         """
         :param **kwargs:
         :param **kwargs:
-        :param input: [?, 21, 80, 200, 1]
+        :param input: [?, 11, 80, 200, 1]
         :return:
         """
         is_training = tf.equal(drop_rate, 0.3)
 
         conv1 = self.conv3d1(inputs)
         conv1 = tf.layers.batch_normalization(conv1, training=is_training)
-        pool1 = self.pooling1(conv1)  # (?, 7, 40, 100, 8)
+        pool1 = self.pooling1(conv1)  # (?, 6, 40, 100, 8)
         # print('pool1: ', pool1.get_shape().as_list())
 
         conv2 = self.conv3d2(pool1)
@@ -84,25 +88,25 @@ class The3dcnn_lstm_Model(tf.keras.Model):
         x_rnns = tf.unstack(x_rnn, axis=-1)  # 展开通道维度  [?, 25, 10] * 4
         x_rnn = tf.concat(x_rnns, axis=-1)  # 合并列维度  [?, 25, 40]
 
-        rnn_output = []
-        for i in range(self.num_class):
-            name = "ltsm_" + str(i)
-            cell = tf.keras.layers.CuDNNLSTM(units=self.rnn_units, name=name)
-            fc = tf.keras.layers.Dense(units=1, use_bias=True, activation=None,
-                                       kernel_initializer=tf.keras.initializers.he_normal(),
-                                       bias_initializer=tf.constant_initializer())
-            drop = tf.keras.layers.Dropout(rate=drop_rate)
-
-            item_out = cell(inputs=x_rnn)  # [?, 64]
-            fc_out = drop(item_out)
-            fc_out2 = fc(fc_out)  # [?, 1]
-            cell = None
-            drop = None
-            fc = None
-
-            rnn_output.append(fc_out2)  # [4, ?, 1]
-        rnn_output = tf.squeeze(rnn_output)  # [4, ?]
-        logits = tf.transpose(rnn_output)  # [?, 4]
+        # rnn_output = []
+        # for i in range(self.num_class):
+        #     name = "ltsm_" + str(i)
+        #     cell = tf.keras.layers.CuDNNLSTM(units=self.rnn_units, name=name)
+        #     fc = tf.keras.layers.Dense(units=1, use_bias=True, activation=None,
+        #                                kernel_initializer=tf.keras.initializers.he_normal(),
+        #                                bias_initializer=tf.constant_initializer())
+        #     drop = tf.keras.layers.Dropout(rate=drop_rate)
+        #
+        #     item_out = cell(inputs=x_rnn)  # [?, 64]
+        #     fc_out = drop(item_out)
+        #     fc_out2 = fc(fc_out)  # [?, 1]
+        #     cell = None
+        #     drop = None
+        #     fc = None
+        #
+        #     rnn_output.append(fc_out2)  # [4, ?, 1]
+        # rnn_output = tf.squeeze(rnn_output)  # [4, ?]
+        # logits = tf.transpose(rnn_output)  # [?, 4]
         ####################################################################
         # rnn_output = []
         # for _index in range(4):
@@ -123,13 +127,8 @@ class The3dcnn_lstm_Model(tf.keras.Model):
         # fc1 = drop(fc1)
         # logits = self.fc2(fc1)
         ####################################################################
-        # cell = tf.keras.layers.CuDNNLSTM(units=self.rnn_units, return_sequences=True)
-        # cell2 = tf.keras.layers.CuDNNLSTM(units=self.rnn_units)
-        # drop = tf.keras.layers.Dropout(rate=drop_rate)
-        #
-        # cell_out1 = cell(x_rnn)
-        # cell_out2 = cell2(cell_out1)
-        # cell_out = drop(cell_out2)
-        # logits = self.fc2(cell_out)
+
+        cell_out1 = self.cell1(x_rnn)
+        logits = self.cell2(cell_out1)
 
         return logits
